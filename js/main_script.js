bc19435a72207f284e1e93c6c7c99ec501da50ee
Your Name <you@example.com>
2021-07-03 01:48:25 +0600

    double page create.

--- js/main_script.js
+++ js/main_script.js
@@ -1,3 +1,8 @@
-/* Script For  Animation */
-     new WOW().init();
-    
+const loginBtn = document.querySelector('.enterBtn');
+loginBtn.addEventListener("click", function (event) {
+     const loginPage = document.querySelector ('.loginArea');
+     loginPage.style.display = "none";
+
+     const frontPage = document.querySelector(".frontPage");
+     frontPage.style.display = "block";
+})
